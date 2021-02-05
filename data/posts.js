e43885a6d472ed09c68148479da756002ccb901e
oscarmanas <oscarmanash@gmail.com>
2021-02-05 14:16:33 +0100

    set up amplify and API

---
+++ data/posts.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 'p1',
+        videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+        user: {
+            id: 'u1',
+            username: 'daviddausa',
+            imageUri: 'https://fotos00.regio7.cat/2020/03/18/690x278/508ad5b8-b93d-42c1-9637-51b96d970998.jpg',
+        },
+        description: 'hahahaha oh boy @borat',
+        song: 'NF - The search',
+        songImage: 'https://fotos00.regio7.cat/2020/03/18/690x278/508ad5b8-b93d-42c1-9637-51b96d970998.jpg',
+        likes: 123,
+        comments: 12,
+        shares: 44,
+    },
+    {
+        id: 'p2',
+        videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+        user: {
+            id: 'u1',
+            username: 'test',
+            imageUri: 'https://fotos00.regio7.cat/2020/03/18/690x278/508ad5b8-b93d-42c1-9637-51b96d970998.jpg',
+        },
+        description: 'hahahaha oh boy @borat',
+        song: 'NF - The search',
+        songImage: 'https://fotos00.regio7.cat/2020/03/18/690x278/508ad5b8-b93d-42c1-9637-51b96d970998.jpg',
+        likes: 123,
+        comments: 12,
+        shares: 44,
+    }
+]
